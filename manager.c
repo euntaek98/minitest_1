199c285220face5e50e88c8c4b022f04e2380975
Kim Eun Taek <21700147@handong.ac.kr>
2023-04-24 15:27:31 +0900

    Add loadData()

--- manager.c
+++ manager.c
@@ -61,7 +61,17 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
+	fp= fopen("product.txt","rt");
+        if (fp == NULL) {
+                printf("File open failed.\n");
+                return 0;
+        }
 
+        while(!feof(fp)) {
+                int result = fscanf(fp, "%s %d %d\n",p[count].name, &p[count].weight, &p[count].price);
+                if (result == EOF) break;
+                count++;
+        }
 
 
 
